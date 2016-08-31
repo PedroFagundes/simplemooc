cb31803a7ca2fa9edd0c179d365d861c27cc5e5f
Pedro Henrique Fagundes <pedrofagundesb@gmail.com>
2016-08-31 18:04:48 -0300

    Changing Django Auth User to custom User

--- simplemooc/simplemooc/accounts/models.py
+++ simplemooc/simplemooc/accounts/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+import re
 
-# Create your models here.
+from django.db import models
+from django.core import validators
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+	username = models.CharField(
+		'Nome de usuário', unique=True, max_length=30,
+		validators=[validators.RegexValidator(re.compile('^[\w.@+-]+$'),
+			'O nome de usuário só pode conter letras, números ou os seguintes caracteres: @/./+/-/_', 'invalid')]
+	)
+	email = models.EmailField('E-mail', unique=True)
+	name = models.CharField('Nome', max_length=100, blank=True)
+	is_active = models.BooleanField('Está ativo?', blank=True, default=True)
+	is_staff = models.BooleanField('É da equipe?', blank=True, default=False)
+	date_joined = models.DateTimeField('Data de Cadastro', auto_now_add=True)
+
+	objects = UserManager()
+
+	USERNAME_FIELD = 'username'
+	REQUIRED_FIELDS = ['email']
+
+	def __unicode__(self):
+		return self.name or self.username
+
+	def get_short_name(self):
+		return self.username
+
+	def get_full_name(self):
+		return str(self)
+
+	class Meta:
+		verbose_name = 'Usuário'
+		verbose_name_plural = 'Usuários'
--- simplemooc/simplemooc/accounts/views.py
+++ simplemooc/simplemooc/accounts/views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from .forms import RegisterForm
+from .forms import RegisterForm, EditAccountForm
 
 
 @login_required
@@ -33,4 +34,29 @@ def register(request):
 @login_required
 def edit(request):
 	template_name = 'accounts/edit.html'
-	return render(request, template_name)
+	form = EditAccountForm()
+	context = {}
+	if request.method == 'POST':
+		form = EditAccountForm(request.POST, instance=request.user)
+		form.save()
+		form = EditAccountForm(instance=request.user)
+		context['success'] = True
+	else:
+		form = EditAccountForm(instance=request.user)
+	context['form'] = form
+	return render(request, template_name, context)
+
+
+@login_required
+def edit_password(request):
+	template_name = 'accounts/edit_password.html'
+	context = {}
+	if request.method == 'POST':
+		form = PasswordChangeForm(data=request.POST, user=request.user)
+		if form.is_valid():
+			form.save()
+			context['success'] = True
+	else:
+		form = PasswordChangeForm(user=request.user)
+	context['form'] = form
+	return render(request, template_name, context)
